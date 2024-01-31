e5362c4819f9813bdfdf4131ebd2cc5115c4645f
tomaspreira08 <tomaspereira04062008@gmail.com>
2024-01-31 22:09:47 +0000

    ajuste dos calculos

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function calcularAgua() {
     var peso = document.getElementById('weight').value;
     
     if (!isNaN(peso) && peso > 0) {
-        var quantidadeAgua = peso * 0.35; // Fórmula ajustada
+        var quantidadeAgua = peso * 0.035; // Fórmula ajustada
 
-        document.getElementById('result').innerHTML = "Você deve beber aproximadamente " + quantidadeAgua.toFixed(2) + " litros de água por dia.";
+        document.getElementById('result').innerHTML = "Você deve beber aproximadamente " + quantidadeAgua.toFixed(3) + " litros de água por dia.";
     } else {
         document.getElementById('result').innerHTML = "Por favor, insira um peso válido.";
     }
